e81b53299607a9db4ec94c0b1da82292cd5c8eb5
Dhanapalnv <76559967+Dhanapalnv@users.noreply.github.com>
2025-02-21 08:57:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 
 
-menu_options = ["Channel_info", "View Table", "List of queries"]
-with st.sidebar:
-    selected = option_menu(None, menu_options, 
-        icons=['info', 'question', "list-task"], 
-        menu_icon="cast", default_index=0, orientation="vertical")
+# menu_options = ["Channel_info", "View Table", "List of queries"]
+st.title("YouTube Channel Data")
